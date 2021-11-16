cf28102342eee2d1a18fd783e6e704309738123e
Sara <saracanovic@gmail.com>
2021-11-16 19:10:29 +0100

    Dodata validacija za login i register

--- Domain/Agent.cs
+++ Domain/Agent.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,8 +14,14 @@ namespace Domain
         public TuristickaAgencija TuristickaAgencija { get; set; }
         public string Ime { get; set; }
         public string Prezime { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
         public string Username { get; set; }
+        //[Required]
+        //[StringLength(18, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        //[RegularExpression(@"^((?=.*[a-z])(?=.*[A-Z])(?=.*\d)).+$")]
+        //[DataType(DataType.Password)]
+        //[Display(Name = "Password")]
         public string Password { get; set; }
     }
 }
--- TravelAgency/Controllers/AgentController.cs
+++ TravelAgency/Controllers/AgentController.cs
@@ -12,7 +12,7 @@ using TravelAgency.Models;
 
 namespace TravelAgency.Controllers
 {
-    [LoggedInAgent]
+    
     public class AgentController : Controller
     {
         private readonly IUnitOfWork uow;
@@ -22,7 +22,7 @@ namespace TravelAgency.Controllers
             this.uow = uow;
         }
         // GET: UserController
-        [NotLoggedIn]
+       
         public ActionResult Index()
         {
             return View("Login");
@@ -33,6 +33,11 @@ namespace TravelAgency.Controllers
         {
             try
             {
+                if (model.Username == null || model.Password == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Morate uneti podatke za sva polja!");
+                    return View();
+                }
                 Agent agent = uow.Agent.GetByUsernameAndPassword(new Agent
                 {
                     Username = model.Username,
@@ -47,30 +52,12 @@ namespace TravelAgency.Controllers
             }
             catch (Exception ex)
             {
-                ModelState.AddModelError(string.Empty, "Losi podaci!");
+                ModelState.AddModelError(string.Empty, "Ovaj agent ne postoji!");
                 return View();
             }
            
         }
-        //[HttpPost]
-        //public ActionResult Login(LoginViewModel model)
-        //{
-        //    try
-        //    {
-        //        User user = unit.Users.GetByUsernameAndPassword(new User { Username = model.Username, Password = model.Password });
-        //        HttpContext.Session.SetInt32("userid", user.UserId);
-        //        HttpContext.Session.SetString("username", user.Username);
-
-        //        HttpContext.Session.Set("user", JsonSerializer.SerializeToUtf8Bytes(user));
-        //        //ViewBag.IsLoggedIn = true;
-        //        return RedirectToAction("Index", "Department");
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        ModelState.AddModelError(string.Empty, "Wrong credentials!");
-        //        return View();
-        //    }
-        //}
+       
 
         // GET: UserController/Details/5
         public ActionResult Details(int id)
@@ -78,7 +65,7 @@ namespace TravelAgency.Controllers
             return View();
         }
 
-        [NotLoggedIn]
+       
         // GET: UserController/Create
         [ActionName("Register")]
         public ActionResult Create()
@@ -92,9 +79,14 @@ namespace TravelAgency.Controllers
         [ActionName("Register")]
         public ActionResult Create(Agent model)
         {
+            if (model.Ime == null || model.Prezime == null || model.Email == null || model.Username == null || model.Password == null)
+            {
+                ModelState.AddModelError(string.Empty, "Morate uneti podatke za sva polja!");
+                return View();
+            }
             if (uow.Agent.Search(u => u.Username == model.Username).Any())
             {
-                ModelState.AddModelError(string.Empty, "Username is taken!");
+                ModelState.AddModelError(string.Empty, "Vec postoji agent sa tim korisnickim imenom!");
                 return View();
             }
             uow.Agent.Add(new Domain.Agent { TuristickaAgencijaID = 1, Ime = model.Ime, Prezime = model.Prezime, Email = model.Email, Username = model.Username, Password = model.Password });
